aa399dea9572dcd468989bf37ded8440bce9704d
Mahasvin24 <mahasvin.manikandan@gmail.com>
2024-02-17 20:28:08 -0800

    CSS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 const Navbar = () => {
   return (
     <div>
-      <div></div>
+      <div>
+        <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+      </div>
+      <div>
+        <h1>TabSurfer</h1>
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
     </div>
   );
 };
