88546ee9fc39699cbbe34182715d2c020d93564e
Justin Lee <justin.huiyun.lee@gmail.com>
2024-02-18 07:25:44 -0800

    made workspaces look cleaner

--- src/components/Workspaces.tsx
+++ src/components/Workspaces.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Input } from "./ui/input";
 import React, { useState, useEffect } from "react";
 import { TbExternalLink } from "react-icons/tb";
@@ -28,49 +28,54 @@ const Workspaces = ({ data }) => {
   }
 
   return (
-    <div className="flex h-full w-full flex-col">
-      <div className="flex justify-between mt-4">
-        <h1 className="flex-grow text-3xl font-medium ml-2">
+    <div className="flex h-full w-full scale-90 flex-col rounded-2xl border p-4 shadow-lg">
+      <div className="mt-4 flex justify-between">
+        <h1 className="ml-2 flex-grow p-4 text-3xl font-medium">
           {data[activeIndex]?.title}
         </h1>
-        <TbExternalLink 
-          className="mr-2 pr-2 text-3xl text-black duration-300 hover:text-gray-500 justify-right" 
-          onClick={() => {openMultipleURLs(data[activeIndex].urls)}}
+        <TbExternalLink
+          className="mr-2 pr-2 text-3xl text-black duration-300 hover:text-gray-500"
+          onClick={() => {
+            openMultipleURLs(data[activeIndex].urls);
+          }}
         />
       </div>
-      <div className="mt-4 h-1 w-full place-self-center bg-black overflow-hidden flex-grow" onClick={openMultipleURLs}></div>
+      <div
+        className="mt-4 h-1 w-full flex-grow place-self-center overflow-hidden bg-black"
+        onClick={openMultipleURLs}
+      ></div>
       <div className="h-[70vh]">
-        {
-          data[activeIndex]?.urls.map((url, index) => (
-            <React.Fragment key={index}>
-              <div className="flex my-4">
-                <h3 className="flex-grow text-md font-medium ml-2 hover:underline text-gray-700 hover:text-black">
-                  {url}
-                </h3>
-                <TbExternalLink 
-                  className="mr-2 pr-2 text-3xl hover:text-black duration-300 text-gray-700 justify-right"
-                  onClick={() => {window.open(url)}} 
-                />
-              </div>
-              <div className="mt-4 h-1 w-full place-self-center rounded-full bg-gray-400"></div>
-            </React.Fragment>
-          ))
-        }
+        {data[activeIndex]?.urls.map((url, index) => (
+          <React.Fragment key={index}>
+            <div className="my-4 flex">
+              <h3 className="text-md ml-2 flex-grow font-medium text-gray-700 hover:text-black hover:underline">
+                {url}
+              </h3>
+              <TbExternalLink
+                className="justify-right mr-2 pr-2 text-3xl text-gray-700 duration-300 hover:text-black"
+                onClick={() => {
+                  window.open(url);
+                }}
+              />
+            </div>
+            <div className="mt-4 h-1 w-full place-self-center rounded-full bg-gray-400"></div>
+          </React.Fragment>
+        ))}
       </div>
       <form onSubmit={formSubmit}>
-          <div className="flex flex-row">
-              <button 
-                className="ml-12 mb-4 mr-2 w-[2.5vw] h-[2.5vw] rounded bg-gray-800 text-3xl text-white duration-300 hover:scale-95"
-                type="submit"
-              >
-                  +
-              </button>
-              <Input
-                  type="text"
-                  placeholder="+ Add a url"
-                  className="w-[70vw] border-black"
-              />
-          </div>
+        <div className="flex flex-row">
+          <Input
+            type="text"
+            placeholder="+ Add a url"
+            className="ml-12 w-[70vw] border-black"
+          />
+          <button
+            className="mb-4 ml-4 mt-0.5 h-[2.5vw] w-[2.5vw] rounded bg-gray-800 text-3xl text-white duration-300 hover:scale-95"
+            type="submit"
+          >
+            +
+          </button>
+        </div>
       </form>
     </div>
   );
